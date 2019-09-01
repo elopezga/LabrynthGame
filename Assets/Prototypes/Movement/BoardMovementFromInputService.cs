513ca370bb6f162b8aab723aefbdadef82735d6d
Edgar Lopez-Garcia <edgarylop@gmail.com>
2019-08-31 22:32:49 -0700

    Code cleanup ✨

--- Assets/Prototypes/Movement/BoardMovementFromInputService.cs
+++ Assets/Prototypes/Movement/BoardMovementFromInputService.cs
@@ -19,21 +19,12 @@ namespace Labrynth.Prototype
             _dataTransformer = GetComponent<BoardMovementFromInputDataTransformer>();
 
             _dataTransformer.BoardMovementReactive.Subscribe(_ => {
-                Debug.Log($"Joystick: {_.Joystick}");
-                Debug.Log($"Camera Forward: {_.CameraForward}");
-                Debug.Log($"Camera Right: {_.CameraRight}");
-
                 Vector3 projectedForward = Vector3.ProjectOnPlane(_.CameraForward, Vector3.up);
                 Vector3 projectedRight = Vector3.ProjectOnPlane(_.CameraRight, Vector3.up);
 
                 transform.rotation = Quaternion.AngleAxis(Joystick2RotationAngle(_.Joystick.x), -1f * projectedForward)
                     * Quaternion.AngleAxis(Joystick2RotationAngle(_.Joystick.y), projectedRight);
             });
-
-            /* _dataTransformer.Joystick.Subscribe(joystick => {
-                transform.rotation = Quaternion.AngleAxis(Joystick2RotationAngle(joystick.x), Vector3.left)
-                    * Quaternion.AngleAxis(Joystick2RotationAngle(joystick.y), Vector3.forward);
-            }); */
         }
 
         private float Joystick2RotationAngle(float joystickAxisValue)
