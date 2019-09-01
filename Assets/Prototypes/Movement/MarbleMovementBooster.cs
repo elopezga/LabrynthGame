2878441da010105f3a93fff8e0be57febb52e1b9
Edgar Lopez-Garcia <edgarylop@gmail.com>
2019-08-31 23:22:53 -0700

    Rudimentary marble input responsiveness improvements

---
+++ Assets/Prototypes/Movement/MarbleMovementBooster.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UniRx;
+
+namespace Labrynth.Prototype
+{
+    public class MarbleMovementBooster : MonoBehaviour
+    {
+        [SerializeField]
+        private JoystickDataStream _joystickStream;
+
+        [SerializeField]
+        private float magnitude;
+
+        private Rigidbody _rigidBody;
+
+        public void Awake()
+        {
+            _rigidBody = GetComponent<Rigidbody>();
+
+            _joystickStream.Joystick.Subscribe(_ => {
+                Vector3 joystick3D = new Vector3(_.x, _.y, 0f);
+                // transform.up should be platform normal
+                Vector3 velocity = Vector3.ProjectOnPlane(joystick3D, transform.up);
+                Debug.DrawRay(transform.position, velocity * magnitude, Color.red);
+                _rigidBody.AddForce(velocity * magnitude);
+            });
+        }
+    }
+}
